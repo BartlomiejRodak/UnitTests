b6f282f9ac72613115ae5cc3d1bb363f05da5a7b
Bart <bartlomiej.rodak93a@gmail.com>
2021-03-30 20:40:16 +0200

    Add two simple tests for OperationService

---
+++ Calculator.Tests/Services/OperationServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Calculator.API.Services;
+using Calculator.API.Services.Abstraction;
+using Xunit;
+
+namespace Calculator.Tests.Services
+{
+    public class OperationServiceTests
+    {
+        private readonly IOperationService sut;
+
+        public OperationServiceTests()
+        {
+            this.sut = new OperationService();
+        }
+
+        [Fact]
+        public void Add_Given_Two_Numbers_When_Add_Then_Return_Correct_Result_Example_With_Normal_Assertion()
+        {
+            // Arrange
+            // Act
+            var result = sut.Add(1, 2);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_Given_Two_Numbers_When_Add_Then_Return_Decimal_Result_Example_With_Normal_Assertion()
+        {
+            // Arrange
+            // Act
+            var result = sut.Add(1, 2);
+
+            // Assert
+            Assert.Equal(typeof(decimal), result.GetType());
+        }
+    }
+}
