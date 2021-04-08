9019e7b4c889ce29b8de5aeb57616578aad16d23
Bart <bartlomiej.rodak93a@gmail.com>
2021-04-08 09:58:51 +0200

    Add Mock with VerifyAny

--- Calculator.Tests/Services/CalculatorServiceTestes.cs
+++ Calculator.Tests/Services/CalculatorServiceTestes.cs
@@ -35,5 +35,23 @@ namespace Calculator.Tests.Services
             result.Should().Be(4);
         }
         #endregion
+
+        #region Verify Any
+        [Fact]
+        public void Calculate_Given_Calculation_Type_And_Numbers_When_Subtract_Then_Subtract_Operation_Was_Called_Once()
+        {
+            // Arrange
+            this.operationServiceMock
+                .Setup(x => x.Subtract(It.IsAny<decimal>(), It.IsAny<decimal>()))
+                .Returns(4);
+
+            // Act
+            this.sut.Calculate(OperationType.Subtract, 2, 2);
+
+            // Assert
+            this.operationServiceMock.Verify(v => v.Subtract(It.IsAny<decimal>(), It.IsAny<decimal>()), Times.Once);
+            this.operationServiceMock.Verify(v => v.Multiply(It.IsAny<decimal>(), It.IsAny<decimal>()), Times.Never);
+        }
+        #endregion
     }
 }
