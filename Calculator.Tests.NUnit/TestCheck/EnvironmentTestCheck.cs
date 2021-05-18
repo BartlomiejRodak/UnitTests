fd203e199d4dd89277a59a36421103fd5d65a3de
Bart <bartlomiej.rodak93a@gmail.com>
2021-05-18 14:02:59 +0200

    Add new Nunit project

---
+++ Calculator.Tests.NUnit/TestCheck/EnvironmentTestCheck.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Calculator.Tests.TestCheck
+{
+    /// <summary>
+    /// Environment configuration check
+    /// </summary>
+    public sealed class EnvironmentCheckTests
+    {
+        [Test]
+        public void EnvironmentTestCheckPositive()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
